bab9d8a070c3151faa75a28cdc39ac9df471affd
Bartosz <123@gmail.net>
2024-03-06 21:57:45 +0100

    zad1

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@
 public class Main {
     public static void main(String[] args) {
 Student BS=new Student("Bartosz","Stefaniak");
+        Student RX=new Student("RURa","xero");
 Teacher MD=new Teacher("Mariusz","Depta");
 Subject math=new Subject("Mathematics",MD);
-
+math.addToClassList(BS);
+math.subjectOut();
     }
 
 }
--- src/Subject.java
+++ src/Subject.java
@@ -3,27 +3,43 @@ import java.util.Scanner;
 public class Subject {
     public static String newline = System.getProperty("line.separator");
 
-        private int NOSTUDS=0;
-        private String subject;
-        private Teacher lecturer;
-        private Student[] classList;
+        private int NOSTUDS;
+        private  String subject;
+        private  Teacher lecturer;
+        private final Student[] classList=new Student[NOSTUDS+2];
 
-        public Subject(String subject, Teacher lecturer) {
+    public Subject(Student[] classList) {
+
+    }
+
+    public Subject(String subject, Teacher lecturer) {
             this.subject = subject;
             this.lecturer = lecturer;
+
+
         }
 
         public void addToClassList(Student stud) {
-classList[NOSTUDS]=stud;
+for(int i=NOSTUDS;i<NOSTUDS+1;i++){
+    classList[i]=stud;
+
+
+}
 NOSTUDS+=1;
         }
     public void subjectOut(){
 
-            System.out.format("Subject's name= %s" + newline + "Teacher's name= %s" + newline );
+            System.out.format("Subject's name= %s " + newline + "Teacher's name= %s %s" + newline ,subject,lecturer.getName(),lecturer.getSurname());
+for(int j=0;j<NOSTUDS;j++){
 
+    System.out.format("Student %d name= %s %s",j,classList[j].getName(),classList[j].getSurname());
+
+
+}
 
 
     }
+
 }
 
 
